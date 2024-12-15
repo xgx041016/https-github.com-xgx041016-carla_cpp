eca65549e97cad08daea185bd10f2fb265861df3
xgx041016 <2305517042@qq.com>
2024-12-15 18:16:54 +0800

    Update Weather.h

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Weather/Weather.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Weather/Weather.h
@@ -3,7 +3,7 @@
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
-
+//设置一个类的声明，包含相关的头文件，并准备编译器生成必要的代码
 #pragma once
 
 #include "GameFramework/Actor.h"
@@ -18,7 +18,7 @@ UCLASS(Abstract)
 class CARLA_API AWeather : public AActor
 {
   GENERATED_BODY()
-
+//定义了一个 AWeather 类的构造函数，接受一个 FObjectInitializer 类型的参数
 public:
 
   AWeather(const FObjectInitializer& ObjectInitializer);
@@ -50,12 +50,12 @@ public:
 
   /// 更新昼夜周期
   void SetDayNightCycle(const bool &active);
-
+//定义了一个受保护（protected）的成员函数 RefreshWeather，并通过 UFUNCTION(BlueprintImplementableEvent) 宏标记它为一个蓝图可实现事件
 protected:
 
   UFUNCTION(BlueprintImplementableEvent)
   void RefreshWeather(const FWeatherParameters &WeatherParameters);
-
+//一个类的私有成员定义部分，涉及天气系统中的一些功能，包括天气效果、后处理材质、以及与昼夜循环相关的设置
 private:
 
   void CheckWeatherPostProcessEffects();
